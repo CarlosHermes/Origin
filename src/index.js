6d553a194680dea5cfbf29da38a5c5e4e698429b
CarlosHermes <61211646+CarlosHermes@users.noreply.github.com>
2020-11-04 17:37:03 +0100

    Added swagger json

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express()
+
+var swaggerUi = require('swagger-ui-express'),
+    swaggerDocument = require('./swagger.json');
+
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.listen(3000, () => {
+    console.log("El servidor está inicializado en el puerto 3000");
+});
