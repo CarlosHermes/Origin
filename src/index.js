fe784d9cecf0946eb51c27745cccbed878e0fb0b
CarlosHermes <61211646+CarlosHermes@users.noreply.github.com>
2020-11-05 15:45:40 +0100

    repared everything
    swagger had errors
    api wasn't connected
    index had redundant code

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const app = express()
+const app = express();
+const api = require('./api');
 
-<script src ="api.js" type="text.js">
-//<script src ="api.js" type="text/javascript"></script>
-//$.getScript("api.js", function(){});
-
-/*var swaggerUi = require('swagger-ui-express'),
-    swaggerDocument = require('./swagger.json');
+var swaggerUi = require('swagger-ui-express');
+var swaggerDocument = require('./swagger.json');
 
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());*/
-
 
 app.listen(3000, () => {
     console.log("El servidor está inicializado en el puerto 3000");
