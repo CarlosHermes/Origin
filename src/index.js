a1b68ddbd47190cddb1ef6718f6f61bd431587d0
CarlosVirus <63506067+CarlosVirus@users.noreply.github.com>
2020-11-29 03:31:12 +0100

    asdasd

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ const express = require("express");
 const app = express();
 const api = require('./api');
 const PORT = process.env.PORT || 3000;
+//const rout = express.Router();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 var swaggerUi = require('swagger-ui-express');
 var swaggerDocument = require('./swagger.json');
 
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 api.myApi(app);
 app.listen(PORT, () => {
