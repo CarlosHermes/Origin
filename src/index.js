d1dbdc72178ec43468cfecd913a06a6ce85d6338
javivong <a19javdedgar@inspedralbes.cat>
2020-12-05 05:16:14 +0100

    Some examples set.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
+//const fetch = require("node-fetch");
 const api = require('./api');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 var path = require('path');
 //const rout = express.Router();
 app.use(bodyParser.urlencoded({ extended: false }));
