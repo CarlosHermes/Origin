af43bdd8b7e3ae561cab0f5b4135f273b9015885
CarlosVirus <63506067+CarlosVirus@users.noreply.github.com>
2020-12-09 13:15:35 +0100

    asdasd

--- src/socket.js
+++ src/socket.js
@@ -1,16 +1,18 @@
-const socket = (app) => {
+const api = require("/apiAlt'");
+socket = (app) => {
     let io = app.get('socketio');
-    let api = app.get('api');
     io.on('connection' , (socket) => {
         console.log('Client connected', socket.id);
-        socket.on('disconnect', () => console.log('Client disconnected'));
+        socket.emit('welcome', 'Bienvenido')
+        socket.on('disconnect', () => console.log('Client disconnected'))
         socket.on('function', (data) =>{
             console.log(data);
             socket.emit('function', data);
         });
-        socket.on('update', (data) => {
-            socket.emit('updateResponse', api.updateData(data));
-        })
+        socket.on('update', data => {
+            api.updateData(data);
+            io.emit('usersUpdate', data);
+        });
     });
 
     setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
